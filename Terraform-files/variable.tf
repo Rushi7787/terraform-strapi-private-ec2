42ff32ad396866f237d5cfe85087fd4d2348b52f
Rushi7787 <rushikeshmshelke@gmail.com>
2026-02-06 19:00:10 +0530

    Create variable.tf

---
+++ Terraform-files/variable.tf
@@ -0,0 +1,33 @@
+variable "aws_region" {
+  type = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+}
+
+variable "public_subnet_cidrs" {
+  description = "Public subnet CIDRs"
+  type        = list(string)
+}
+
+variable "private_subnet_cidrs" {
+  description = "Private subnet CIDRs"
+  type        = list(string)
+}
+
+variable "availability_zones" {
+  description = "Availability zones"
+  type        = list(string)
+}
+
+variable "ami_id" {
+  description = "AMI ID for EC2"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+}
